4789535bb05cd653f2edc5fee2261e971c6591de
ToastyTad <toastytadgit@gmail.com>
2017-09-17 19:54:21 -0400

    Added gui functionality.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 200));
+        primaryStage.setScene(new Scene(root, 490, 200));
         primaryStage.resizableProperty().setValue(false);
         primaryStage.show();
     }
