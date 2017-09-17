302c5ed1282f0bd3892c9d65b2bcbde8a28137ec
ToastyTad <toastytadgit@gmail.com>
2017-09-17 19:53:56 -0400

    Added some general unit converter tests.

--- test/UnitConverterTests.java
+++ test/UnitConverterTests.java
@@ -1,10 +1,6 @@
-import javafx.fxml.Initializable;
-import measurement.Length;
-import measurement.Measurement;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;import javafx.fxml.Initializable;
+import measurement.*;
 import sample.Controller;
-
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +11,50 @@ public class UnitConverterTests
     {
         Controller testController = new Controller();
         assertTrue(testController instanceof Initializable);
+    }
+
+    @Test
+    public void testListViewContainsLength()
+    {
+        boolean hasInstance = false;
+        Controller testController = new Controller();
+        Measurement[] items = testController.formListView();
+        for (Measurement m : items)
+        {
+            hasInstance = m instanceof Length;
+            if (hasInstance)
+                break;
+        }
+        assertTrue(hasInstance);
+    }
+
+    @Test
+    public void testListViewContainsMass()
+    {
+        boolean hasInstance = false;
+        Controller testController = new Controller();
+        Measurement[] items = testController.formListView();
+        for (Measurement m : items)
+        {
+            hasInstance = m instanceof Mass;
+            if (hasInstance)
+                break;
+        }
+        assertTrue(hasInstance);
+    }
+
+    @Test
+    public void testListViewContainsTime()
+    {
+        boolean hasInstance = false;
+        Controller testController = new Controller();
+        Measurement[] items = testController.formListView();
+        for (Measurement m : items)
+        {
+            hasInstance = m instanceof Time;
+            if (hasInstance)
+                break;
+        }
+        assertTrue(hasInstance);
     }
 }
